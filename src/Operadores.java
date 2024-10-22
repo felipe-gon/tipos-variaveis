7e97fb5ae8d10c5feaf6056cf0ebf71270221949
felipe-gon <felipev2014@hotmail.com>
2024-10-22 07:47:29 -0300

    Testando commit

---
+++ src/Operadores.java
@@ -0,0 +1,14 @@
+public class Operadores {
+    public static void main(String[] args) {
+        int numero = 5;
+
+        numero = - numero; // Para negar um número, utilizar o operador - antes da variável
+            System.out.println(numero);
+
+        numero = numero * -1; // Para converter um que está negativo em positivo, multiplicar por -1
+            System.out.println(numero);
+
+        numero = - numero;
+            System.out.println(numero);
+    }
+}
---
+++ src/tipoVariaveis.java
@@ -0,0 +1,10 @@
+public class tipoVariaveis {
+    public static void main(String[] args) {
+        short numeroCurto = 2;
+        int numeroInteiro = 84;
+        final double valorPi = 3.14;
+
+        numeroInteiro = (numeroInteiro / numeroCurto * (int) valorPi);
+            System.out.print(numeroInteiro);
+    }
+}
