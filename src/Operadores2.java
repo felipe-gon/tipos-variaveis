8f8b614711c89d8684a1efd91ff0fedc66549ed3
felipe-gon <felipev2014@hotmail.com>
2024-10-22 21:59:03 -0300

    Commit 2

---
+++ src/Operadores2.java
@@ -0,0 +1,10 @@
+public class Operadores2 {
+    public static void main(String[] args) {
+        int numero = 2;
+            numero++;
+
+        String resultado = numero > 2 ?"verdadeiro" : "falso";
+
+            System.out.println(resultado);
+    }
+}
